b7b76e932baab70d35d4f3a93f482041a2e94ffd
董宏锋 <602029096@qq.com>
2017-12-07 14:30:17 +0800

    完善部分模块  v0.4

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,9 @@
 export const basicConfig = {
-    // API_HOST: 'http://apit.fruitswl.com/api'
-    API_HOST: 'http://192.168.1.225/api'
+    API_HOST: 'http://apit.fruitswl.com/api'
+}
+
+let _host = window.location.host
+
+if(_host === 'localhost:8089') {
+    basicConfig.API_HOST = 'http://192.168.1.225/api'
 }
